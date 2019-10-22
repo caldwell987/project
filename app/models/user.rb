0e2f0baa3fb149a8866ed8ab427d2361318559a1
fernandocgomez <fernandocgomez@live.com>
2019-10-22 14:57:30 -0500

    14:57 oct 22

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
-    has_many :contacts
-    has_many :friends
-    has_secure_password
+    
+     #Relationships
+     has_many :contacts
+     has_many :friends
+ 
+     #Password validation
+     has_secure_password
+ 
+     #To make sure user is filling out the whole form
+     validates :username, :lastname, :firstname, :password_digest, presence: true 
+     validates :password_digest, confirmation: true 
+     validates :username, length: { in: 6..12 } 
+     validates :username, uniqueness: true 
+ 
 end
